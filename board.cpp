0e0a112a42a89c8cc63ee5f97b99db9c097d8cf3
Luke Schaefer <luke.schaefer@mail.missouri.edu>
2023-04-24 11:44:31 -0500

    update - removed forward checking

--- board.cpp
+++ board.cpp
@@ -1,35 +1,115 @@
 #include "board.hpp"
 #include "helpers.hpp"
 
+// initial setup: 2D array of numbers representing a sudoku board
 SudokuBoard::SudokuBoard(int initial_setup[9][9]) {
-    openSpaces = 9 * 9;
     for(int row = 0; row < 9; row++) {
         for(int col = 0; col < 9; col++) {
-            board[row][col] = Box(initial_setup[row][col], row, col);
+            board[row][col] = initial_setup[row][col];
+        }
+    }
+}
 
-            if(initial_setup[row][col] != 0) {
-                openSpaces--;
-                rowDictionary[row].push_back(initial_setup[row][col]);
-                colDictionary[col].push_back(initial_setup[row][col]);
-                gridDictionary[calcGridNum(row, col)].push_back(initial_setup[row][col]);
+void SudokuBoard::printBoard() {
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool SudokuBoard::safeMove(int row, int col, int value) {
+    if(value < 1 || value > 9) {
+        throw "Invalid value.";
+    }
+    if(row < 0 || row > 9 || col < 0 || col > 9) {
+        throw "Invalid coordinantes.";
+    }
+
+    // checks if number is already in the row
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == value) {
+            return false;
+        }      
+    }
+    // checks if the number is already in the col
+    for (int i = 0; i < 9; i++) {
+        if (board[i][col] == value) {
+            return false;
+        }  
+            
+    }
+
+    // check if number is already in the particular 3*3 matrix
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if (board[i + startRow][j + startCol] == value) {
+                return false;
             }
         }
     }
+    return true;
+}
 
-    for(int row = 0; row < 9; row++) {
-        for(int col = 0; col < 9; col++) {
-            updateDomains(row, col);
-            updateDegrees(row, col);
-        }
+// recursive algorithm in which solves the sudoku board with backtracking
+bool SudokuBoard::backtrackingSolve(int row, int col) {
+
+    // we have reached the very end with no errors, so it is solved!
+    if (row == 8 && col == 9)
+        return true;
+
+    // if we are on the last column, we move to the next row
+    if (col == 9) {
+        row++;
+        col = 0;
     }
+
+    // if there is already a number here, move to the next space
+    if (board[row][col] > 0)
+        return backtrackingSolve(row, col + 1);
+
+    for (int value = 1; value <= 9; value++) 
+    {
+        // Check if it is safe to place the num (1-9) in the given row and col
+        // we then move to next column
+        if (safeMove(row, col, value)) 
+        {
+              
+           /* Assigning the num in 
+              the current (row,col)
+              position of the grid
+              and assuming our assigned 
+              num in the position
+              is correct     */
+            board[row][col] = value;
+            
+            //  Checking the next possibility with next column
+            if (backtrackingSolve(row, col + 1)) {
+                return true;
+            }
+                
+        }
+        // Removes the guessed value since our assumption was wrong,
+        // so we go for the next assumption with a diff num value
+        board[row][col] = 0;
+    }
+    return false;
     
 }
 
-void SudokuBoard::updateDomains(int row, int col) {
-    
-}  
+void SudokuBoard::solveBoard() {
+    std::cout << "Initial Board: " << std::endl;
+    printBoard();
 
-
-void SudokuBoard::updateDegrees(int row, int col) {
-
+    if(backtrackingSolve(0,0)) {
+        std::cout << "Solved board: " << std::endl;
+        printBoard();
+    }
+    else {
+        std::cout << "No solution found. Unsolvable!" << std::endl;
+    }
 }
--- board.hpp
+++ board.hpp
@@ -3,27 +3,19 @@
 // Each square is represented as a box object from box.hpp
 #ifndef BOARD.HPP
 #define BOARD.HPP
-#include <map>
-#include <vector>
-#include "box.hpp"
+#include <iostream>
 
 class SudokuBoard {
     private:
-        // holds the information for each row/col/grid values already in the board for quick access
-        std::map<int, std::vector<int>> gridDictionary;
-        std::map<int, std::vector<int>> rowDictionary;
-        std::map<int, std::vector<int>> colDictionary;
-
-        // number of open spaces still on the board
-        int openSpaces;
-
         // the actual board containing the boxes and box data
-        Box board[9][9];
+        int board[9][9];
 
     public:
         SudokuBoard(int initial_board[9][9]); 
-        void updateDomains(int row, int col);
-        void updateDegrees(int row, int col);
+        void printBoard();
+        bool safeMove(int row, int col, int value);
+        bool backtrackingSolve(int row, int col);
+        void solveBoard();
 };
 
 #endif
